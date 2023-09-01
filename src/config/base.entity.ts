8a38671cc7880277b29a6ce141befe44de6196a7
Nehuen Cisneros <cisneros.n.b@outlook.com>
2023-09-01 01:24:56 -0300

    mysql funcionando

---
+++ src/config/base.entity.ts
@@ -0,0 +1,23 @@
+//id
+//created_ad
+//updated_ad
+
+import { CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+export abstract class BaseEntity {
+  
+  @PrimaryGeneratedColumn("uuid")
+  id!: string;
+
+  @CreateDateColumn({
+    name: "created_ad",
+    type: "timestamp",
+  })
+  createdAd!: Date;
+
+  @UpdateDateColumn({
+    name: "updated_ad",
+    type: "timestamp",
+  })
+  updatedAt!: Date;
+}
---
+++ src/entities/user.entity.ts
@@ -0,0 +1,17 @@
+import { Column, Entity } from "typeorm";
+import { BaseEntity } from "../config/base.entity"
+
+@Entity({name: "user"})
+export class UserEntity extends BaseEntity {
+
+  @Column()
+  username!: string;
+
+  @Column()
+  name!: string;
+
+  @Column()
+  lastName!: string;
+
+
+}
